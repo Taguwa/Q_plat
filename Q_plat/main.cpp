6ae9df0bb809b9c8ba9f5e1e09f5bf985401b8dd
Taguwa <tagucraft@gmail.com>
2022-10-06 11:30:32 +0900

    Initial Commit

---
+++ Q_plat/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Q_plat
+//
+//  Created by Taguwa on 2022/10/06.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
